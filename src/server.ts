fb4dd9dbb26c8ba3e7c9ab1e35aad91308741f52
guilhermedomeneghini <guilherme.walmeling@gmail.com>
2025-05-25 12:43:37 -0300

    feat: Rota criar contato

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import helmet from 'helmet'
+import router from './routes';
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(express.urlencoded({extended:true}))
+
+server.use('/', router);
+
+const porta = 3000;
+server.listen(porta, () => {
+    console.log(`Servidor rodando na porta: http://localhost:${porta}`)
+})
